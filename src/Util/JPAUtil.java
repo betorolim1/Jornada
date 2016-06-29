bc01704339f6b9891899308465e1b0a2fd6bd826
Adalberto <betorolim1@gmail.com>
2016-06-29 15:00:48 -0300

    Arrumando import

--- src/Util/JPAUtil.java
+++ src/Util/JPAUtil.java
@@ -1,9 +1,9 @@
 package Util;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
-import javax.faces.bean.ApplicationScoped;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
