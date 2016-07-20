f0868d58a21df6373a631e0735aa062d38517a18
Adalberto <betorolim1@gmail.com>
2016-07-20 13:32:52 -0300

    Extraindo logica de login para loginBean

---
+++ src/br/com/betorolim/loja/bean/LoginBean.java
@@ -0,0 +1,76 @@
+package br.com.betorolim.loja.bean;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import br.com.betorolim.loja.dao.UsuarioDao;
+import br.com.betorolim.loja.modelo.Usuario;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+
+	@Inject
+	private Usuario usuario;
+
+	@Inject
+	private UsuarioDao dao;
+	
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	
+	public boolean isLogado() {
+		if (usuario.getLogin() == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public void login() throws IOException {
+		boolean login = dao.existe(usuario);
+		System.out.println(login);
+		if (login == true) {
+			FacesContext.getCurrentInstance().getExternalContext().redirect("principal.xhtml");
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_FATAL, "Login Invalido", null));
+			this.usuario = new Usuario();
+		}
+	}
+	
+	public void menuLogin() throws IOException {
+		if (this.isLogado()) {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario já logado", null));
+		} else {
+			FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+		}
+	}
+
+	public void menuCarrinho() {
+
+	}
+	
+	public void menuLogout() throws IOException {
+		if (this.isLogado()) {
+			this.usuario = new Usuario();
+			FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+		} else {
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario não está logado", null));
+		}
+
+	}
+	
+}
--- src/br/com/betorolim/loja/bean/UsuarioBean.java
+++ src/br/com/betorolim/loja/bean/UsuarioBean.java
@@ -2,9 +2,8 @@ package br.com.betorolim.loja.bean;
 
 import java.io.IOException;
 
+import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.persistence.PersistenceException;
@@ -13,14 +12,16 @@ import br.com.betorolim.loja.dao.UsuarioDao;
 import br.com.betorolim.loja.modelo.Usuario;
 
 //@Model //@RequestScoped e @Named
-@ManagedBean
-@SessionScoped
+@Model
 public class UsuarioBean {
 
 	private Usuario usuario = new Usuario();
 
 	@Inject
 	private UsuarioDao dao;
+	
+	@Inject
+	private LoginBean loginBean;
 
 	public Usuario getUsuario() {
 		return usuario;
@@ -30,44 +31,12 @@ public class UsuarioBean {
 		this.usuario = usuario;
 	}
 
-	public boolean isLogado() {
-		if (usuario.getLogin() == null) {
-			return false;
-		} else {
-			return true;
-		}
-	}
-
-	public void menuLogin() throws IOException {
-		if (this.isLogado()) {
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario já logado", null));
-		} else {
-			FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
-		}
-	}
-
-	public void menuCarrinho() {
-
-	}
-
-	public void menuLogout() throws IOException {
-		if (this.isLogado()) {
-			this.usuario = new Usuario();
-			FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
-		} else {
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_INFO, "Usuario não está logado", null));
-		}
-
-	}
-
 	public void cadastrar() throws IOException {
 		try {
 			if (!dao.existePorNome(usuario)) {
 				if (!dao.existePorEmail(usuario)) {
 					dao.adiciona(usuario);
-					FacesContext.getCurrentInstance().getExternalContext().redirect("principal.xhtml");
+					FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
 				} else {
 					FacesContext.getCurrentInstance().addMessage(null,
 							new FacesMessage(FacesMessage.SEVERITY_FATAL, "E-mail ja utilizado", null));
@@ -79,18 +48,6 @@ public class UsuarioBean {
 		} catch (PersistenceException e) {
 			FacesContext.getCurrentInstance().addMessage(null,
 					new FacesMessage(FacesMessage.SEVERITY_FATAL, "Não autorizado", null));
-		}
-	}
-
-	public void login() throws IOException {
-		boolean login = dao.existe(usuario);
-		System.out.println(login);
-		if (login == true) {
-			FacesContext.getCurrentInstance().getExternalContext().redirect("principal.xhtml");
-		} else {
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(FacesMessage.SEVERITY_FATAL, "Login Invalido", null));
-			this.usuario = new Usuario();
 		}
 	}
 
