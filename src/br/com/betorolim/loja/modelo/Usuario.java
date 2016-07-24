e8d15df2de0089c84daf2fee9dc41f1b2a078331
Adalberto <betorolim1@gmail.com>
2016-07-24 15:54:04 -0300

    Perfil não pode ser nulo

--- src/br/com/betorolim/loja/modelo/Usuario.java
+++ src/br/com/betorolim/loja/modelo/Usuario.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
@@ -26,8 +27,9 @@ public class Usuario implements Serializable {
 	@Size(min=4, max=20, message="Senha deve ter no minimo 4 e no maximo 20 caracteres")
 	private String senha;
 	@Email
-	@NotEmpty(message="Não deve ser vazio")
+	@NotEmpty(message="Email não deve ser vazio")
 	private String email;
+	@NotNull(message="Perfil não deve ser vazio")
 	private Perfil perfil;
 	
 	public Long getId() {
