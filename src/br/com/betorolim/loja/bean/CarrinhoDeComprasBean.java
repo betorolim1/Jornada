40c55e92963e07a6cf14cb9ef1a7a9e592f77de9
Adalberto <betorolim1@gmail.com>
2016-08-10 10:53:13 -0300

    Carrinho de compras (incompleto)

--- src/br/com/betorolim/loja/bean/CarrinhoDeComprasBean.java
+++ src/br/com/betorolim/loja/bean/CarrinhoDeComprasBean.java
@@ -1,10 +1,13 @@
 package br.com.betorolim.loja.bean;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 import br.com.betorolim.loja.modelo.Livro;
@@ -17,14 +20,12 @@ public class CarrinhoDeComprasBean implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private LoginBean usuarioLogado = new LoginBean();
 
 	private Double total = 0.0;
 
-	private List<Livro> livros = new ArrayList<Livro>(); // Ver tipo comprado
-															// (Ebook, combo,
-															// impresso)
-
-	private Livro livro = new Livro();
+	private List<Livro> livros = new ArrayList<Livro>(); 
 
 	public Double getTotal() {
 		return total;
@@ -42,11 +43,20 @@ public class CarrinhoDeComprasBean implements Serializable {
 		this.livros = livros;
 	}
 
-	public String adicionaEbook(Livro livro) {
-		livro.setTipoComprado("Ebook");
-		livros.add(livro);
-		total += livro.getPrecoEbook();
-		return "principal?faces-redirect=true";
+	public void adicionaEbook(Livro livro) throws IOException {
+//		System.out.println(usuarioLogado.getUsuario());
+//		if(usuarioLogado.getUsuario()!=null){
+			livro.setTipoComprado("Ebook");
+			livros.add(livro);
+			total += livro.getPrecoEbook();
+			
+			FacesMessage msg = new FacesMessage("Livro adicionado",null);
+	        FacesContext.getCurrentInstance().addMessage(null, msg);
+			FacesContext.getCurrentInstance().getExternalContext().redirect("principal.xhtml");
+//		}else{
+//			FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_WARN,"Faça login para comprar",null);
+//	        FacesContext.getCurrentInstance().addMessage(null, msg);
+//		}
 	}
 
 	public String adicionaImpresso(Livro livro) {
