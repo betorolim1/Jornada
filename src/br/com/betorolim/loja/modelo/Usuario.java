756e7834c490e34bf8f15bf006067af307837194
Adalberto <betorolim1@gmail.com>
2016-08-11 15:53:17 -0300

    Implementando finalizar compra (Incompleto)

--- src/br/com/betorolim/loja/modelo/Usuario.java
+++ src/br/com/betorolim/loja/modelo/Usuario.java
@@ -1,17 +1,12 @@
 package br.com.betorolim.loja.modelo;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
